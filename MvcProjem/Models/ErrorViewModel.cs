01efe30649c56f144a56e7ab19d18b8097737eb7
Suat Adalı <suatadl@gmail.com>
2023-09-18 21:50:52 +0300

    ilk projem

---
+++ MvcProjem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcProjem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
